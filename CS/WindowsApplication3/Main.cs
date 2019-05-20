72bf8f1d6166218f113fdde50bf7defd36c773b4
Donate_ro <joke4789@ya.ru>
2019-05-20 18:01:18 +0300

    DataSource is replaced from the database file to the dynamical DataTable object

--- CS/WindowsApplication3/Main.cs
+++ CS/WindowsApplication3/Main.cs
@@ -1,34 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 using DevExpress.XtraEditors;
 using DevExpress.XtraGrid;
-using DevExpress.XtraGrid.Columns;
+using System;
+using System.Data;
+using System.Windows.Forms;
 
 
-namespace DXSample {
-    public partial class Main: XtraForm {
-        public Main() {
+namespace DXSample
+{
+    public partial class Main : XtraForm
+    {
+        public Main()
+        {
             InitializeComponent();
         }
-        public void InitData() {
-            for(int i = 0;i < 5;i++) 
-                dataSet11.Tables[0].Rows.Add(new object[] { i, string.Format("FirstName {0}", i), i, DateTime.Today.AddDays(i), true });
+
+        DataTable GetCustomerInfoDataTable()
+        {
+            DataTable table = new DataTable();
+            table.TableName = "CustomerInfo";
+            table.Columns.Add(new DataColumn("CustomerID", typeof(int)));
+            table.Columns.Add(new DataColumn("FirstName", typeof(string)));
+            table.Columns.Add(new DataColumn("LastName", typeof(string)));
+            table.Columns.Add(new DataColumn("Date", typeof(DateTime)));
+            table.Columns.Add(new DataColumn("Check", typeof(bool)));
+            for(int i = 0; i < 5; i++)
+            {
+                table.Rows.Add(i, string.Format("FirstName {0}", i), i, DateTime.Today.AddDays(i), i % 2 == 0);
+            }
+            return table;
         }
 
-        private void OnLoad(object sender, EventArgs e) {
-            InitData();
-            gridControl.ForceInitialize();
-            for(int i = 0;i < gridView.VisibleColumns.Count;i++)
-                gridView.VisibleColumns[i].RealColumnEdit.EditValueChanged += OnEditValueChanged;
-        }
-
-        private void OnEditValueChanged(object sender, EventArgs e) {
-            if(gridView.FocusedRowHandle == GridControl.NewItemRowHandle) {
+        private void OnEditValueChanged(object sender, EventArgs e)
+        {
+            if(gridView.FocusedRowHandle == GridControl.NewItemRowHandle)
+            {
                 gridView.CloseEditor();
                 gridView.UpdateCurrentRow();
                 gridView.ShowEditor();
@@ -38,10 +43,24 @@ namespace DXSample {
                 edit.SelectionLength = 0;
             }
         }
-        protected override void OnFormClosing(FormClosingEventArgs e) {
-            for(int i = 0;i < gridView.VisibleColumns.Count;i++)
+        private void OnLoad(object sender, EventArgs e)
+        {
+            InitData();
+            gridControl.ForceInitialize();
+            for(int i = 0; i < gridView.VisibleColumns.Count; i++)
+                gridView.VisibleColumns[i].RealColumnEdit.EditValueChanged += OnEditValueChanged;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            for(int i = 0; i < gridView.VisibleColumns.Count; i++)
                 gridView.VisibleColumns[i].RealColumnEdit.EditValueChanged -= OnEditValueChanged;
             base.OnFormClosing(e);
+        }
+
+        public void InitData()
+        {
+            customerInfoBindingSource.DataSource = GetCustomerInfoDataTable();
         }
     }
 }
